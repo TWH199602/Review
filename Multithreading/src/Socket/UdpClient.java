0640a3e9b0104ed6ed49157fda8466ed17bf16a5
TENwh <695126460@qq.com>
2020-06-02 20:48:19 +0800

    Socket Practice

--- Multithreading/src/Socket/UdpClient.java
+++ Multithreading/src/Socket/UdpClient.java
@@ -11,12 +11,21 @@ public class UdpClient {
 
   public static void main(String[] args) throws IOException {
     DatagramSocket ds = new DatagramSocket();
-    String word = "你好";
-    byte[] bytes = word.getBytes();
-    DatagramPacket dp = new DatagramPacket(bytes, bytes.length, InetAddress.getByName("192.168.118.2"),
-        10000);
-    ds.send(dp);
-
+    System.out.println("发送数据，输入fart结束:");
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    String line = null;
+    while ((line = bufferedReader.readLine()) != null) {
+      System.out.println("发送数据，输入fart结束:");
+      byte[] buf = line.getBytes();
+      DatagramPacket dp = new DatagramPacket(buf, buf.length,
+          InetAddress.getByName("192.168.11.213"),
+          10000);
+      ds.send(dp);
+      if("fart".equals(line)){
+        System.out.println("停止发送");
+        break;
+      }
+    }
     ds.close();
   }
 }
