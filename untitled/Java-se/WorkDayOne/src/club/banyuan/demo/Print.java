202cc7e51bc964e23de23e8d82b9cc69525e9d4b
TENwh <695126460@qq.com>
2020-06-08 20:22:40 +0800

    work&note-6-6

--- untitled/Java-se/WorkDayOne/src/club/banyuan/demo/Print.java
+++ untitled/Java-se/WorkDayOne/src/club/banyuan/demo/Print.java
@@ -10,10 +10,10 @@ public class Print {
     a[1] = new String[]{"金", "木", "水", "火", "土"};
     a[2] = new String[]{"天", "地"};
     System.out.println(a);
-    for (int i = 0; i < a.length; i++) {
-      for (String j : a[i])
-        System.out.print(j + " ");
-      System.out.println();
+    for (int i = 0; i < a.length; i++) { //遍历每行数组的长度
+      for (String j : a[i]) //遍历每行数组的元素
+        System.out.print(j + " "); //打印每行数组的元素；print与println的区别：前者输出后不换行，后者输出后换行；
+      System.out.println();//打印完一行后换行；
     }
 
   }
