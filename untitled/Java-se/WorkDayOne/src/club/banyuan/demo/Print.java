9c84b567d4cf8fece9ec51670bd2fbf7c10020f0
TENwh <695126460@qq.com>
2020-06-08 20:26:54 +0800

    work&note-6-6

--- untitled/Java-se/WorkDayOne/src/club/banyuan/demo/Print.java
+++ untitled/Java-se/WorkDayOne/src/club/banyuan/demo/Print.java
@@ -12,7 +12,9 @@ public class Print {
     System.out.println(a);
     for (int i = 0; i < a.length; i++) { //遍历每行数组的长度
       for (String j : a[i]) //遍历每行数组的元素
+      {
         System.out.print(j + " "); //打印每行数组的元素；print与println的区别：前者输出后不换行，后者输出后换行；
+      }
       System.out.println();//打印完一行后换行；
     }
 
